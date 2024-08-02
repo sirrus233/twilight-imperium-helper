57e8593313425e2715afc16b6b7b67511346280b
Chelsea Schmidt <cschmidt331@gmail.com>
2024-08-01 20:55:21 -0700

    Added aws deployment infrastructure

---
+++ infrastructure/lib/infrastructure-stack.ts
@@ -0,0 +1,44 @@
+import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as cloudfront_origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
+
+export class InfrastructureStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const originAccessIdentity = new cloudfront.OriginAccessIdentity(
+            this,
+            "OriginAccessIdentity"
+        );
+
+        const bucket = new s3.Bucket(this, "Bucket");
+        bucket.grantRead(originAccessIdentity);
+
+        const certificate = certificatemanager.Certificate.fromCertificateArn(
+            this,
+            "Certificate",
+            "arn:aws:acm:us-east-1:017820696103:certificate/367a1d2f-13f9-4fd8-a7cb-d38220abfefe"
+        );
+
+        const distribution = new cloudfront.Distribution(this, "Distribution", {
+            defaultRootObject: "index.html",
+            defaultBehavior: {
+                origin: new cloudfront_origins.S3Origin(bucket, {
+                    originAccessIdentity,
+                }),
+            },
+            certificate: certificate,
+            domainNames: [
+                "*.bradleysherman.net/twilightimperiumtools",
+                "bradleysherman.net/twilightimperiumtools",
+            ],
+        });
+
+        new cdk.CfnOutput(this, "DistributionDomainName", {
+            value: distribution.distributionDomainName,
+        });
+    }
+}
