1a284441439c187a43249e8c5f3f760de9f76716
Bradley Sherman <sirrus233@gmail.com>
2024-08-10 04:45:57 -0700

    settings and region correcT

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -5,5 +5,5 @@ import { InfrastructureStack } from "../lib/infrastructure-stack";
 
 const app = new cdk.App();
 new InfrastructureStack(app, "InfrastructureStack", {
-    env: { account: "017820696103", region: "us-east-1" },
+    env: { account: "017820696103", region: "us-west-2" },
 });
