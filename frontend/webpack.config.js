9805dcb366df0a4aae890c0d906e4e9dca23dc2b
Chelsea Schmidt <cschmidt331@gmail.com>
2024-09-28 10:54:22 -0700

    .ico format instead of .png

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./public/index.html",
-            favicon: "./public/favicon.png",
+            favicon: "./public/favicon.ico",
         }),
         new MiniCssExtractPlugin(),
     ],
